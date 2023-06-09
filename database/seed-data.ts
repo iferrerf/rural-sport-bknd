14b7f70d1ce190e604121713a31d2033d1cb6035
iferrerf <iferrerf@iesch.org>
2023-06-09 13:57:18 +0200

    Actualizacion de imagenes de pistas de padel

--- database/seed-data.ts
+++ database/seed-data.ts
@@ -22,8 +22,8 @@ export const initialData: SeedData = {
         nombre: 'Pista de pádel',
         lugar: 'Mirambel',
         images: [
-            'https://estaticos-cdn.sport.es/clip/06d09b0b-a9a3-4328-838b-a49ce536de82_alta-libre-aspect-ratio_default_0.jpg',
-            'https://www.qualitysportinstalacionesdeportivas.com/wp-content/uploads/2019/10/fabrica-de-pistas-de-padel.jpg',
+            'https://as01.epimg.net/masdeporte/imagenes/2020/05/11/polideportivo/1589206126_472503_1589206640_noticia_normal.jpg',
+            'https://www.elbierzodigital.com/wp-content/uploads/2022/05/PADEL2.jpg',
         ],
         descripcion: 'Pista de césped artificial al aire libre, situada a las afueras de la localidad, a escasos 400m de Mirambel. Ubicada junto a la piscina municipal y el campo de multideporte.',
         temporada: 'Todo el año',
@@ -35,8 +35,8 @@ export const initialData: SeedData = {
         nombre: 'Pista de pádel',
         lugar: 'Iglesuela del Cid',
         images: [
-            'https://as01.epimg.net/masdeporte/imagenes/2020/05/11/polideportivo/1589206126_472503_1589206640_noticia_normal.jpg',
-            'https://www.elbierzodigital.com/wp-content/uploads/2022/05/PADEL2.jpg',
+            'https://estaticos-cdn.sport.es/clip/06d09b0b-a9a3-4328-838b-a49ce536de82_alta-libre-aspect-ratio_default_0.jpg',
+            'https://www.qualitysportinstalacionesdeportivas.com/wp-content/uploads/2019/10/fabrica-de-pistas-de-padel.jpg',
         ],
         descripcion: 'Pista al aire libre, situada en un extremo de la localidad, en la salida hacia la ermita del Cid. Ubicada junto al campo de fútbol y al frontón.',
         temporada: 'Todo el año',
