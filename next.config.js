7789acbf4cbab5e49e98333d0b67b804998aaa4b
iferrerf <iferrerf@iesch.org>
2023-06-13 12:58:04 +0200

    configuracion cors next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/api/pistas',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://rural-sport-bknd.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
